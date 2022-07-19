80725e46549e2d31bea4ce388dfd797ddae04b8f
YoungWoongKim9644 <duddnd904@gmail.com>
2022-07-19 18:57:23 +0900

    [build] Domain Entity 기초 설정

---
+++ Backend/src/main/java/com/ssafy/domain/user/User.java
@@ -0,0 +1,45 @@
+package com.ssafy.domain.user;
+
+//import com.ssafy.domain.rating.Rating;
+import com.ssafy.domain.rating.Rating;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "USER_ID" ,insertable = false, updatable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String phone;
+    @Column(nullable = false)
+    private boolean phone_auth;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false)
+    private String account;
+    @Column(nullable = false)
+    private int grade;
+    @Column(nullable = false)
+    private String aboutMe;
+    @Column(nullable = false)
+    private LocalDateTime data_create;
+
+    @OneToMany(mappedBy = "user")
+    List<Rating> ratings = new ArrayList<>();
+
+}
