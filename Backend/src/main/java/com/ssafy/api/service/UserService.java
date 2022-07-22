ea0e1b095f573e645c30ee74732c68174faffb72
TaeHwan <wjd30142@naver.com>
2022-07-22 16:49:40 +0900

    [feat] Back 회원탈퇴, 회원탈퇴 테스트코드

--- Backend/src/main/java/com/ssafy/api/service/UserService.java
+++ Backend/src/main/java/com/ssafy/api/service/UserService.java
@@ -1,9 +1,12 @@
 package com.ssafy.api.service;
 
+import com.ssafy.api.request.UserLoginPostReq;
 import com.ssafy.api.request.UserRegisterPostReq;
 import com.ssafy.domain.user.User;
 
 public interface UserService {
     User createUser(UserRegisterPostReq userRegisterInfo);
     User getUserByPhone(String phone);
+
+    boolean deleteUser(UserLoginPostReq deleteInfo);
 }
--- Backend/src/test/java/com/ssafy/api/controller/UserControllerTest.java
+++ Backend/src/test/java/com/ssafy/api/controller/UserControllerTest.java
@@ -2,7 +2,10 @@ package com.ssafy.api.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.api.request.UserLoginPostReq;
 import com.ssafy.api.request.UserRegisterPostReq;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +15,10 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
+@Transactional
 public class UserControllerTest {
 
     @Autowired
@@ -45,6 +51,49 @@ public class UserControllerTest {
                 )
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void withdrawal() throws Exception {
+        UserRegisterPostReq userRegisterPostReq = new UserRegisterPostReq();
+        userRegisterPostReq.setPhone("010-0000-0000");
+        userRegisterPostReq.setPassword("your_password");
+        userRegisterPostReq.setName("홍길동");
+        userRegisterPostReq.setAddress("협의가 필요합니다");
+        userRegisterPostReq.setAccount("110-342-143345");
+
+        // 테스트를 위한 회원가입 데이터
+        mockMvc.perform(
+                post("/api/v1/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(userRegisterPostReq))
+        );
+
+        UserLoginPostReq userLoginPostReq = new UserLoginPostReq();
+        userLoginPostReq.setPhone("010-0000-0001");
+        userLoginPostReq.setPassword("your_password");
+
+        // 탈퇴 실패
+        mockMvc.perform(
+                        delete("/api/v1/user")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(toJson(userLoginPostReq))
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+
+        userLoginPostReq.setPhone("010-0000-0000");
+
+        // 탈퇴 성공
+        mockMvc.perform(
+                delete("/api/v1/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(userLoginPostReq))
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+
     }
 
     private <T> String toJson(T data) throws JsonProcessingException {
