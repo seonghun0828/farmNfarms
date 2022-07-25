9a725b8d096352f1633292022484e6272c04bf6d
SSAFY <zow777@naver.com>
2022-07-25 18:00:27 +0900

    test commit

--- frontend/src/components/AuctionRoom.jsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import { OpenVidu } from "openvidu-browser";
-
---
+++ frontend/src/components/AuctionRoom/StreamComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StreamComponent = (props) => {
+
+}
+
+export default StreamComponent;
