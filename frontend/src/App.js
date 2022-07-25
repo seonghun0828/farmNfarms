cfd4af9b96eb15b8adb41b2a58aff45a540b8458
imukyee <imukyee@gmail.com>
2022-07-25 16:08:51 +0900

    [chore] Front: 테스트 코드 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './components/pages/Home'
 import Login from './components/pages/Login'
 import Join from './components/pages/Join'
-import Input from './components/atoms/Input'
+
 function App() {
   return (
     <div className="App">
-      <Input size={'small'}/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
