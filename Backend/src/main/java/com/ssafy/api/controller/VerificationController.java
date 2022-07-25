f441d91c0f5e6ff523c44ae2ccbe713589f750e5
DESKTOP-KVCQHCD\SSAFY <duddnd904@gmail.com>
2022-07-25 15:58:25 +0900

    [feat] back Verification Swagger 적용

--- Backend/src/main/java/com/ssafy/api/controller/VerificationController.java
+++ Backend/src/main/java/com/ssafy/api/controller/VerificationController.java
@@ -14,12 +14,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("/apis")
 @RequiredArgsConstructor
+@Api(value = "휴대폰 인증 API", tags = {"Verification"})
 public class VerificationController {
 
     private final VerificationService verificationService;
 
     @PostMapping("/verifications")
-    @ApiOperation(value = "인증을 만든다.", notes = "인증을 만든다니까요")
+    @ApiOperation(value = "인증 객체 생성", notes = "입력된 휴대전화을 통해 인증 번호를 발송한다.")
 
     @ApiResponses({
             @ApiResponse(code = 200, message = "성공")
@@ -30,6 +31,12 @@ public class VerificationController {
     }
 
     @PostMapping("verifications/{id}")
+    @ApiOperation(value = "인증 번호 입력", notes = "발송된 인증 번호를 입력하여 휴대폰 인증 과정을 진행한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "번호 중복"),
+            @ApiResponse(code = 402, message = "인증 실패")
+    })
     public ResponseEntity<Map<String, Integer>> confirmVerification(@PathVariable Long id, @RequestBody ConfirmVerificationDto confirmVerificationDto) {
 
         boolean isDuplicated = verificationService.isDuplicated(id);
@@ -42,15 +49,15 @@ public class VerificationController {
         // 2 이면 인증코드 fail
 
         if(isDuplicated) {
-            jsonMap.put("isSuccess", 1);
+            jsonMap.put("isSuccess", 401);
             return ResponseEntity.ok(jsonMap);
         }
         else if(!isConfirmed){
-            jsonMap.put("isSuccess",2);
+            jsonMap.put("isSuccess",402);
             return ResponseEntity.ok(jsonMap);
         }
 
-        jsonMap.put("isSuccess",0);
+        jsonMap.put("isSuccess",200);
         return ResponseEntity.ok(jsonMap);
     }
 }
