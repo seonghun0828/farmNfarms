970fbc5711b362ffc168b6df25f1c52e08c68958
imukyee <imukyee@gmail.com>
2022-07-27 05:08:08 +0900

    [feat] Front: atom 컴포넌트들 수정 및 InputButton 제작

---
+++ frontend/src/components/molecules/InputButton/index.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled, { css } from "styled-components";
+import { FlexBox, Label, StyledInput, HelpText } from "../../atoms/Input/Input.styled";
+import Button from "../../atoms/Button"
+
+const STATUS = {
+  DEFAULT: "default",
+  READONLY: "readOnly",
+  ERROR: "error",
+};
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0.2rem;
+`;
+
+const InputButton = ({ 
+  status, 
+  label, 
+  helpMsg,
+  type,
+  placeholder, 
+  btnMsg,
+  btnFontSize,
+  mode,
+  btnWidth,
+  ...rest 
+}) => {
+  return (
+    <FlexBox>
+      <Label status={status} {...rest}>
+        {label}
+      </Label>
+      <Layout>
+        <StyledInput 
+          status={status} 
+          type={type} 
+          placeholder={placeholder} 
+          {...rest}
+        >
+        </StyledInput>
+        <Button 
+          fontSize={btnFontSize} 
+          mode={mode} 
+          width={btnWidth} 
+          {...rest}
+        >
+          {btnMsg}
+        </Button>
+      </Layout>
+      <HelpText status={status} {...rest}>
+        {helpMsg}
+      </HelpText>
+    </FlexBox>
+  );
+}
+
+InputButton.defaultProps = {
+  status: STATUS.DEFAULT,
+};
+
+export default InputButton;
