0473f64e3d54467226e32d3f8604a9625c35edf3
seonghun <pch468@naver.com>
2022-07-27 11:15:51 +0900

    [feat] Frontend : add Login Page component

--- frontend/src/components/pages/Login/index.jsx
+++ frontend/src/components/pages/Login/index.jsx
@@ -1,17 +1,42 @@
 import React from "react";
-import Checkbox from "../../atoms/Checkbox";
 import Image from "../../atoms/Image";
-import Label from "../../atoms/Label";
 import CheckboxLabel from "../../molecules/CheckboxLabel";
+import Button from '../../atoms/Button'
+import logo from '../../../static/temp_logo.png'
+import styled, { css } from "styled-components";
+import Text from "../../atoms/Text";
+import Input from '../../atoms/Input'
+
+const flex = css`
+    ${({theme}) => 
+        css`
+            ${theme.flex.columnCenter}
+        `
+    }
+`
+const StyledLogin = styled.div`
+    ${flex}
+    justify-content: space-around;
+    height:70vh;
+`
+const AskJoin = styled.div`
+
+`
 
 const Login = () => {
-    return <>
-        <h1>Login Page</h1>
-        <Label htmlFor="one" color='green3' size="xxl" >sdfafweagaeghef</Label>
-        <Checkbox id="one" name='check' value="aha"  size='xxxl' />
-        <Image src='https://images.unsplash.com/photo-1658732049821-0e5ce13224cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60' alt='moutain' size='xxxl' />
+    return <StyledLogin>
+        <Button>뒤로 가기</Button>
+        <Image src={logo} alt='logo' size='xxxl' />
+        <Text fontSize='xxxl'>로그인</Text>
+        <Input label="휴대전화 번호" />
+        <Input label="비밀번호" />
         <CheckboxLabel text="휴대전화 번호 저장" size='lg' id="phone-save" />
-    </>
+        <Button size='sm'>로그인</Button>
+        <AskJoin>
+            <Text fontSize='sm'>계정이 아직 없으신가요?</Text>
+            <Button>회원가입</Button>
+        </AskJoin>
+    </StyledLogin>
 }
 
 export default Login;
