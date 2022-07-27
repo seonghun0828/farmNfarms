65d5bab044dabedfe5c281df1a9a84a622bf7901
seonghun <pch468@naver.com>
2022-07-27 12:49:34 +0900

    [feat] Frontend : update Button in Login

--- frontend/src/components/pages/Login/index.jsx
+++ frontend/src/components/pages/Login/index.jsx
@@ -2,30 +2,37 @@ import React from "react";
 import Image from "../../atoms/Image";
 import CheckboxLabel from "../../molecules/CheckboxLabel";
 import Button from '../../atoms/Button'
-import logo from '../../../static/temp_logo.png'
+import logo from '../../../assets/temp_logo.png'
 import styled, { css } from "styled-components";
 import Text from "../../atoms/Text";
 import Input from '../../atoms/Input'
 
-const flex = css`
+const flexColumn = css`
     ${({theme}) => 
         css`
             ${theme.flex.columnCenter}
         `
     }
 `
+const flexRow = css`
+    ${({theme}) => 
+        css`
+            ${theme.flex.rowCenter}
+        `
+    }
+`
 const StyledLogin = styled.div`
-    ${flex}
+    ${flexColumn}
     justify-content: space-around;
     height:70vh;
 `
 const AskJoin = styled.div`
-
+    ${flexRow}
 `
 
 const Login = () => {
     return <StyledLogin>
-        <Button>뒤로 가기</Button>
+        <Button fontSize='lg' mode='primary'>뒤로 가기</Button>
         <Image src={logo} alt='logo' size='xxxl' />
         <Text fontSize='xxxl'>로그인</Text>
         <Input label="휴대전화 번호" />
@@ -34,7 +41,7 @@ const Login = () => {
         <Button size='sm'>로그인</Button>
         <AskJoin>
             <Text fontSize='sm'>계정이 아직 없으신가요?</Text>
-            <Button>회원가입</Button>
+            <Button mode='primary'>회원가입</Button>
         </AskJoin>
     </StyledLogin>
 }
