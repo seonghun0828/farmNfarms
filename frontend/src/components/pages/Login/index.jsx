3ccb92a0b5cdff0a65204c60afb0dc072e7c4e7b
seonghun <pch468@naver.com>
2022-07-27 13:44:39 +0900

    [feat] Frontend : update Login Page component

--- frontend/src/components/pages/Login/index.jsx
+++ frontend/src/components/pages/Login/index.jsx
@@ -24,24 +24,43 @@ const flexRow = css`
 const StyledLogin = styled.div`
     ${flexColumn}
     justify-content: space-around;
-    height:70vh;
+    height:80vh;
 `
+
+const LoginInput = styled.div`
+    width: 20rem;
+`
+
 const AskJoin = styled.div`
     ${flexRow}
 `
 
+const LeftAlign = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    width: 20rem;
+`
+
 const Login = () => {
     return <StyledLogin>
-        <Button fontSize='lg' mode='primary'>뒤로 가기</Button>
+        <LeftAlign>
+            <Button fontSize='lg' mode='graytext'>뒤로 가기</Button>
+        </LeftAlign>
         <Image src={logo} alt='logo' size='xxxl' />
-        <Text fontSize='xxxl'>로그인</Text>
-        <Input label="휴대전화 번호" />
-        <Input label="비밀번호" />
-        <CheckboxLabel text="휴대전화 번호 저장" size='lg' id="phone-save" />
-        <Button size='sm'>로그인</Button>
+        <LeftAlign>
+            <Text color='green5' weight='bold' fontSize='xxxl'>로그인</Text>
+        </LeftAlign>
+        <LoginInput>
+            <Input label="휴대전화 번호" />
+            <Input label="비밀번호" />
+        </LoginInput>
+        <LeftAlign>
+            <CheckboxLabel text="휴대전화 번호 저장" size='lg' id="phone-save" />
+        </LeftAlign>
+        <Button width='20rem' mode='primary'>로그인</Button>
         <AskJoin>
-            <Text fontSize='sm'>계정이 아직 없으신가요?</Text>
-            <Button mode='primary'>회원가입</Button>
+            <Text fontSize='sm'>계정이 아직 없으신가요? &nbsp;</Text>
+            <Button fontSize='md' mode='graytext'>회원가입하기</Button>
         </AskJoin>
     </StyledLogin>
 }
