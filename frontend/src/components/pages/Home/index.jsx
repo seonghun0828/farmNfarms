5491a6a9610bf05993bb47c1c9e6c1c9f54c3728
seonghun <pch468@naver.com>
2022-07-29 18:07:45 +0900

    [feat] Frontend : update Main page UI

--- frontend/src/components/pages/Home/index.jsx
+++ frontend/src/components/pages/Home/index.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import RoomCard from "../../molecules/RoomCard";
 import logo from '../../../assets/temp_logo.png';
+import styled from "styled-components";
+import Navbar from '../../molecules/Navbar'
+import SearchBar from '../../molecules/SearchBar'
+import Text from '../../atoms/Text'
 
+const StyledHome = styled.div`
+
+`
+const FlexSearchArea = styled.div`
+    ${({theme}) => theme.flex.rowCenter}
+    height: 5rem;
+`
+const SearchArea = styled.div`
+    width: 90%;
+`
 const Home = () => {
-    // return <h1>Home Page</h1>
-    return <>
+    return (
+    <StyledHome>
+        <Navbar url={logo} isLogin imgSize='xs' fontSize='sm' mode='graytext' />
+        <FlexSearchArea>
+            <SearchArea>
+                <SearchBar />
+            </SearchArea>
+        </FlexSearchArea>
+        <Text>빨리 들어와유</Text>
         <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-    </>
+    </StyledHome>)
 }
 
 export default Home;
