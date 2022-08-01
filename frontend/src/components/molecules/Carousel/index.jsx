645f49c8791834687ac02b89bfb9abb4a23aac0d
imukyee <imukyee@gmail.com>
2022-08-01 13:27:55 +0900

    [feat] Front: 캐러셀 제작중

--- frontend/src/components/molecules/Carousel/index.jsx
+++ frontend/src/components/molecules/Carousel/index.jsx
@@ -2,10 +2,9 @@ import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import RoomCard from "../RoomCard";
 import logo from '../../../assets/temp_logo.png';
-import Slide from './slide'
 
 const Container = styled.div`
-  width: 60%;
+  width: 100%;
   overflow: hidden; // 선을 넘어간 이미지들은 보이지 않도록 처리합니다.
 `;
 const Button = styled.button`
@@ -25,8 +24,13 @@ const SliderContainer = styled.div`
   display: flex;
 `;
 
-const TOTAL_SLIDES = 5;
-export default function Slider() {
+const SlidePadding = styled.div`
+  padding-right: 1rem;
+`
+
+const TOTAL_SLIDES = 4;
+
+const Carousel = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slideRef = useRef(null);
 
@@ -46,28 +50,33 @@ export default function Slider() {
 
 useEffect(() => {
   slideRef.current.style.transition = "all 0.5s ease-in-out";
-  slideRef.current.style.transform = `translateX(-${currentSlide*8}rem)`; // 백틱을 사용하여 슬라이드로 이동하는 애니메이션을 만듭니다.
+  slideRef.current.style.transform = `translateX(-${currentSlide*9}rem)`; // 백틱을 사용하여 슬라이드로 이동하는 애니메이션을 만듭니다.
 }, [currentSlide]);
 
 return (
   <Container>
     {currentSlide}
     <SliderContainer ref={slideRef}>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
-      <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
+      <SlidePadding>
+        <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
+      </SlidePadding>
+      <SlidePadding>
+        <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
+      </SlidePadding>
+      <SlidePadding>
+        <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
+      </SlidePadding>
+      <SlidePadding>
+        <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
+      </SlidePadding>
+      <SlidePadding>
+        <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
+      </SlidePadding>
     </SliderContainer>
     <Button onClick={prevSlide}>Previous Slide</Button>
     <Button onClick={nextSlide}>Next Slide</Button>
   </Container>
-);
+  );
 }
+
+export default Carousel;
--- frontend/src/components/pages/Home/index.jsx
+++ frontend/src/components/pages/Home/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import RoomCard from "../../molecules/RoomCard";
 import logo from '../../../assets/temp_logo.png';
+import Carousel from '../../molecules/Carousel'
+
 
 const Home = () => {
     // return <h1>Home Page</h1>
     return <>
         <RoomCard profileImg={logo} headerSize='xxs' viewerSize='sm' title='배추아저씨' num='13' thumnail='https://img.seoul.co.kr//img/upload/2020/07/22/SSI_20200722215818.jpg'/>
+        <Carousel></Carousel>
     </>
 }
 
