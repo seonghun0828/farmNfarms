f1ab112a1eb09872d7e751777185f84ea52c8a34
SSAFY <zow777@naver.com>
2022-08-03 15:02:30 +0900

    [feat]front: 경매방 기능 테스트

--- frontend/src/components/auction/VideoRoomComponent.jsx
+++ frontend/src/components/auction/VideoRoomComponent.jsx
@@ -190,7 +190,7 @@ const VideoRoomComponent = (props) => {
             videoSource: videoDevices[0].deviceId, // The source of video. If undefined default webcam
             publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
             publishVideo: true, // Whether you want to start publishing with your video enabled or not
-            resolution: '390x844', // The resolution of your video
+            resolution: '400x720', // The resolution of your video
             frameRate: 30, // The frame rate of your video
             insertMode: 'APPEND', // How the video is inserted in the target element 'video-container'
             mirror: true, // Whether to mirror your local video or not
