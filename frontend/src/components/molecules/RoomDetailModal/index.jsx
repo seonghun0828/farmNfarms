3f5d62b2e1d7a3721f06ce2759490fffe83c5a73
imukyee <imukyee@gmail.com>
2022-08-09 10:34:57 +0900

    [feat] 방 카드 & 방상세모달 API 연동

--- frontend/src/components/molecules/RoomDetailModal/index.jsx
+++ frontend/src/components/molecules/RoomDetailModal/index.jsx
@@ -7,8 +7,7 @@ import CloseIcon from '@mui/icons-material/Close';
 import ReadItemCard from "../ReadItemCard";
 
 // 위로 올라오듯이 css 효과 넣기
-// div padding 같은거 통일해서 넣기
-// 상태를 메인페이지에서 관리하는게 맞나?? (리덕스 고려하기)
+
 const BackGround = styled.div`
   position: fixed;
   background-color: rgba(0, 0, 0, 0.4);
@@ -26,7 +25,7 @@ const Content = styled.div`
   margin: auto;
   background-color: white;
   width: 100%;
-  height: 80vh;
+  height: 65vh;
   border-radius: 1rem 1rem 0 0;
   box-shadow: 0 0 1rem 0.2rem rgba(0, 0, 0, 0.3);
 `;
@@ -34,7 +33,7 @@ const Content = styled.div`
 const Header = styled.div`
   background-color: ${theme.colors.green5};
   width: 100%;
-  height: 8%;
+  height: 9%;
   border-radius: 1rem 1rem 0 0;
   display: flex;
   justify-content: space-between; 
@@ -44,10 +43,10 @@ const Header = styled.div`
 // 왜 알파벳은 개행 안되냐거~~
 const RoomDetailSection = styled.div`
   width: 100%;
-  height: 80%;
+  height: 82%;
   overflow-x: hidden;
   overflow-y: auto;
-  padding: 0 1rem 0.5rem 1rem;
+  padding: 0 1rem 2rem 1rem;
 `
 
 // 개행이 됐었는데 안됐습니다
@@ -94,7 +93,7 @@ const ButtonSection = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 10%;
+  height: 9%;
   background-color: white;
 `
 const Div = styled.div`
@@ -109,44 +108,26 @@ const Div = styled.div`
   padding-right: ${(props) => props.pr + 'rem'};
 `;
 
-const EXAMPLE_ITEMS = [
-  {
-    id: '1',
-    imageUrl: "https://img.hankyung.com/photo/202010/01.24087325.1.jpg",
-    product: "배추",
-    quantity: "1000kg",
-    grade: "최상",
-    startingPrice: "100,000원"
-  },
-  {
-    id: '2',
-    imageUrl: "https://img.hankyung.com/photo/202010/01.24087325.1.jpg",
-    product: "감자",
-    quantity: "2000kg",
-    grade: "최상",
-    startingPrice: "200,000원"
-  },
-  {
-    id: '3',
-    imageUrl: "https://img.hankyung.com/photo/202010/01.24087325.1.jpg",
-    product: "옥수수",
-    quantity: "500kg",
-    grade: "중상",
-    startingPrice: "80,000원"
-  },
-]
-
 const RoomDetailModal = ({title, description, items, closeModal}) => {
   return (
     <ModalPortal>
-      <BackGround onClick={closeModal}/>
+      <BackGround onClick={(e) => {
+        closeModal();
+        e.stopPropagation();
+      }}/>
       <Content>
         <Header>
           <Div pl={1}>
             <Text color="white" size="xxl" weight="bold">방 상세 정보</Text>
           </Div>
           <Div pr={1}>
-            <CloseIcon onClick={closeModal} style={{cursor: 'pointer', color: 'white'}}/>
+            <CloseIcon 
+              onClick={(e) => {
+                closeModal();
+                e.stopPropagation();
+              }} 
+              style={{cursor: 'pointer', color: 'white'}}
+            />
           </Div>
         </Header>
         <RoomDetailSection>
@@ -170,7 +151,10 @@ const RoomDetailModal = ({title, description, items, closeModal}) => {
           <ItemsSection>
             <Text color="gray2" size="xl" weight="bold">항목</Text>
             <ItemCardSection>
-              {items.map((item) => <ReadItemCard {...item} key={item.id}/>)}                         
+              {items ? 
+                items.map((item) => <ReadItemCard {...item} key={item.id}/>) :
+                <div>isLoading</div>
+              }                        
             </ItemCardSection>
           </ItemsSection>
           </Div>
@@ -182,6 +166,30 @@ const RoomDetailModal = ({title, description, items, closeModal}) => {
     </ModalPortal>
   );
 }
+
+const EXAMPLE_ITEMS = [
+  {
+    id: '1',
+    productTitle: "배추",
+    quantity: "1000kg",
+    grade: "최상",
+    startingPrice: "100,000원"
+  },
+  {
+    id: '2',
+    productTitle: "감자",
+    quantity: "2000kg",
+    grade: "최상",
+    startingPrice: "200,000원"
+  },
+  {
+    id: '3',
+    productTitle: "당근",
+    quantity: "500kg",
+    grade: "중상",
+    startingPrice: "80,000원"
+  },
+]
 
 RoomDetailModal.defaultProps = {
   items: EXAMPLE_ITEMS,
