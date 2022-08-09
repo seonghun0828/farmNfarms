93524b3e187bef8f27c6bd99dae4e55c3a4b0822
seonghun <pch468@naver.com>
2022-08-10 00:51:13 +0900

    [fix] Frontend : update codes

--- frontend/src/components/pages/CreateAuctionRoom/index.jsx
+++ frontend/src/components/pages/CreateAuctionRoom/index.jsx
@@ -159,8 +159,9 @@ const CreateAuctionRoom = () => {
       formData.append('img', file);
       thumbnailIdx = await uploadFile(formData);
     }
-    if (await createAuctionRoom(title, description, thumbnailIdx, items, phone) {
+    if (await createAuctionRoom(title, description, thumbnailIdx, items, phone)) {
       // 경매방으로 navigate
+      console.log('경매방 생성 성공');
     }
   }
   return (
