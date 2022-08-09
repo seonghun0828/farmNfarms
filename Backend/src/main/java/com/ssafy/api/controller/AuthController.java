b7d11c7197361bf3b6ee83284dadb5c6dbb05914
TaeHwan <wjd30142@naver.com>
2022-08-10 01:07:40 +0900

    [fix] Back : HttpOnly Test

--- Backend/src/main/java/com/ssafy/api/controller/AuthController.java
+++ Backend/src/main/java/com/ssafy/api/controller/AuthController.java
@@ -66,7 +66,7 @@ public class AuthController {
             Cookie cookie=new Cookie("refreshToken", refreshToken); // refresh 담긴 쿠키 생성
             cookie.setMaxAge(JwtTokenUtil.refreshExpirationTime); // 쿠키의 유효시간을 refresh 유효시간만큼 설정
             cookie.setSecure(true); // 클라이언트가 HTTPS가 아닌 통신에서는 해당 쿠키를 전송하지 않도록 하는 설정
-            cookie.setHttpOnly(true); // 브라우저에서 쿠키에 접근할 수 없도록 하는 설정
+            //cookie.setHttpOnly(true); // 브라우저에서 쿠키에 접근할 수 없도록 하는 설정
             cookie.setPath("/");
             
             response.addCookie(cookie);
