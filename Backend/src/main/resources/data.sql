fc6c9e222704aa257562b06fcfb575f5248f28e2
DESKTOP-KVCQHCD\SSAFY <duddnd904@gmail.com>
2022-08-10 14:36:45 +0900

    [fix] 백엔드 : data.sql 추가

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO IMAGE (origin_file_name, server_file_name, file_path, content_type, file_size)
+INSERT INTO IMAGE (content_type, file_path, file_size, origin_file_name, server_file_name)
 VALUES
-('string', 'string1', 'string', 'string', 100),
-('string', 'string2', 'string', 'string', 100),
+('image/jpeg', '/root/pictures/202208101037266079035057.jpg', 88821, 'karina.jpg', '202208101037266079035057.jpg'),
+('image/png', '/root/pictures/202208101037502688086397.png', 21058, 'dog.png', '202208101037502688086397.png'),
 ('string', 'string3', 'string', 'string', 100),
 ('string', 'string4', 'string', 'string', 100),
 ('string', 'string5', 'string', 'string', 100),
@@ -17,9 +17,9 @@ VALUES ('110-342-143345', 'SSAFY', '홍길동', '1','01000000001', now(), 1);
 
 INSERT INTO AUCTION_ROOM (created_at, auction_room_description, thumbnail_id, auction_room_title, auctioned, owner_id)
 VALUES
-(now(), 'string1', 1, 'string', false, 1),
-(now(), 'string2', 2, 'string', false, 1),
-(now(), 'string3', 3, 'string', false, 1),
+(now(), '감자감자방', 1, '1번 감자방', false, 1),
+(now(), 'string2', 2, '2번 열정 농부방', false, 1),
+(now(), 'string3', 3, '3번 멋쟁이 포도방', false, 1),
 (now(), 'string4', 4, 'string', false, 1),
 (now(), 'string5', 5, 'string', false, 1),
 (now(), 'string6', 6, 'string', false, 1),
