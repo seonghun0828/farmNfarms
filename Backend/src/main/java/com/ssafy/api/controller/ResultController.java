d4d12de14d3422e279d2898f619022b5ed03678d
Hyejin <heo015414@gmail.com>
2022-08-12 01:51:52 +0900

    [feat] back: auctionResult 상태 변경 구현

--- Backend/src/main/java/com/ssafy/api/controller/ResultController.java
+++ Backend/src/main/java/com/ssafy/api/controller/ResultController.java
@@ -6,9 +6,7 @@ import com.ssafy.api.response.GetAllHistoryRes;
 import com.ssafy.api.response.GetBuyHistoryDetailRes;
 import com.ssafy.api.response.GetSellHistoryDetailRes;
 import com.ssafy.api.service.CreateAuctionResultService;
-import com.ssafy.api.service.GetHistoryService;
-import com.ssafy.domain.auctionResult.AuctionResult;
-import com.ssafy.domain.auctionResult.AuctionResultRepository;
+import com.ssafy.api.service.HistoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +24,7 @@ public class ResultController {
 
     private final CreateAuctionResultService createAuctionResultService;
 
-    private final GetHistoryService getHistoryService;
+    private final HistoryService historyService;
 
     @PostMapping()
     @ApiOperation(value = "경매결과 생성", notes = "전달받은 request에 따른 경매 결과를 생성합니다.")
@@ -45,29 +43,39 @@ public class ResultController {
     @GetMapping("/sell")
     @ApiOperation(value = "판매이력 전체조회", notes = "전달받은 유저의 phoneNumber로 판매이력을 조회합니다.")
     public ResponseEntity<List<GetAllHistoryRes>> getSellHistory(String phoneNumber) {
-        return ResponseEntity.ok(getHistoryService.getAllSellHistory(phoneNumber));
+        return ResponseEntity.ok(historyService.getAllSellHistory(phoneNumber));
     }
 
     @GetMapping("/sell/detail")
     @ApiOperation(value = "판매내역 상세조회", notes = "전달받은 경매결과의 id를 통해 판매내역을 상세 조회합니다.")
     public ResponseEntity<GetSellHistoryDetailRes> getSellHistoryDetail(Long auctionResultId) {
-        return ResponseEntity.ok(getHistoryService.getSellHistoryDetail(auctionResultId));
+        return ResponseEntity.ok(historyService.getSellHistoryDetail(auctionResultId));
     }
 
     @GetMapping("/buy")
     @ApiOperation(value = "구매이력 전체조회", notes = "전달받은 유저의 phoneNumber로 구매이력을 조회합니다.")
     public ResponseEntity<List<GetAllHistoryRes>> getBuyHistory(String phoneNumber) {
-        return ResponseEntity.ok(getHistoryService.getAllBuyHistory(phoneNumber));
+        return ResponseEntity.ok(historyService.getAllBuyHistory(phoneNumber));
     }
 
 
     @GetMapping("/buy/detail")
     @ApiOperation(value = "구매내역 상세조회", notes = "전달받은 경매결과의 id를 통해 구매내역을 상세 조회합니다.")
     public ResponseEntity<GetBuyHistoryDetailRes> getBuyHistoryDetail(Long auctionResultId) {
-        return ResponseEntity.ok(getHistoryService.getBuyHistoryDetail(auctionResultId));
+        return ResponseEntity.ok(historyService.getBuyHistoryDetail(auctionResultId));
     }
 
+    @GetMapping()
+    @ApiOperation(value = "상태 변경", notes = "전달받은 경매결과의 id에 해당하는 객체의 deal, delivery 상태를 업데이트합니다.")
+    public ResponseEntity<HashMap<String, Boolean>> updateDeliveryAndDealState(Long auctionResultId) {
+        boolean isSuccess = historyService.updateDeliveryAndDealState(auctionResultId);
 
+        HashMap<String, Boolean> jsonMap = new HashMap<>();
 
+        if(isSuccess) jsonMap.put("success", true);
+        else jsonMap.put("success", false);
+        return ResponseEntity.ok(jsonMap);
+
+    }
 
 }
