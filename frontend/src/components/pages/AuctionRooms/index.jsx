e65d499d6c014a274712dac5c67e0a140552576f
seonghun <pch468@naver.com>
2022-08-16 15:05:06 +0900

    [fix] Frontend : fix reissue logic

--- frontend/src/components/pages/AuctionRooms/index.jsx
+++ frontend/src/components/pages/AuctionRooms/index.jsx
@@ -11,8 +11,6 @@ import RoomCard from '../../molecules/RoomCard';
 import { LocalRecorder } from 'openvidu-browser';
 import searchAuctionRooms from './searchAuctionRooms';
 import Text from '../../atoms/Text';
-import { useDispatch } from 'react-redux';
-import reissue from '../../../common/reissue'
 
 const StyledAuctionRooms = styled.div`
   display: flex;
