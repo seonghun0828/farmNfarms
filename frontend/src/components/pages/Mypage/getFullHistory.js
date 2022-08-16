7839332a0ba4b18da12e90b5ed8518d3bfe6fc49
seonghun <pch468@naver.com>
2022-08-16 17:10:02 +0900

    [fix] Frontend : test to refresh in Mypage

--- frontend/src/components/pages/Mypage/getFullHistory.js
+++ frontend/src/components/pages/Mypage/getFullHistory.js
@@ -3,6 +3,10 @@ import { alertError } from '../../../common/alertError';
 import apiPath from '../../../common/apiPath';
 
 const getFullHistory = async (phoneNumber, isSalesHistory) => {
+  if (!phoneNumber)
+    return {
+      data: []
+    }
   const path = isSalesHistory ? apiPath.result.sell() : apiPath.result.buy();
   try {
     const { data } = await axios({
