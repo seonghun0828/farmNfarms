d7569d6fbb90441fe290d8fc21302ab317995912
imukyee <imukyee@gmail.com>
2022-08-17 17:04:35 +0900

    [style] Front: sweetAlert 추가

--- frontend/src/components/pages/PayResult/Success/index.jsx
+++ frontend/src/components/pages/PayResult/Success/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Button from '../../../atoms/Button';
 import paySuccess from './paySuccess';
+import Swal from "sweetalert2";
+import theme from '../../../../common/theme';
 
 const Success = () => {
   const params = new URLSearchParams(window.location.search);
   const pg_token = params.get('pg_token');
   paySuccess(pg_token).then(() => {
-    window.alert('결제가 성공했습니다. 홈으로 이동합니다.')
+    Swal.fire({
+      text: '결제가 성공되었습니다! 홈으로 이동합니다.',
+      confirmButtonColor: theme.colors.green3, 
+    })
+    //window.alert('결제가 성공했습니다. 홈으로 이동합니다.')
     window.location.href = '/';
   });
   return (
